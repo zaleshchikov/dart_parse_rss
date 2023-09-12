caeca41945ed503299a5ae9d0de5a664f634b2d3
zaleshchikov <zaleshchikovaa@gmail.com>
2023-09-12 10:59:45 +0500

    основна

---
+++ bin/main.dart
@@ -0,0 +1,39 @@
+
+
+import 'package:dart_rss/domain/media/title.dart';
+import 'package:http/http.dart' as http;
+import 'package:dart_rss/dart_rss.dart';
+
+void main() {
+  final client = http.Client();
+
+  // RSS feed
+  client
+      .get(Uri.parse('https://1tourtv.online/blog-list/feed/'))
+      .then((response) {
+    return response.body;
+  }).then((bodyString) {
+    final channel = RssFeed.parse(bodyString);
+    //print(channel.items.first.content!.value);
+    final firstNews = channel.items.first.content!.value;
+    final listOfFirstNews = firstNews.split('<p>');
+    listOfFirstNews[0] = listOfFirstNews[0].substring(listOfFirstNews[0].indexOf('src=')+5, listOfFirstNews[0].length - listOfFirstNews[0].indexOf('src=')+5);
+    //print(listOfFirstNews[0].substring(0, listOfFirstNews[0].indexOf("\"")));
+
+    final title = channel.items.first.title;
+    final description = channel.items.first.description!.split('</p>')[0].replaceAll('<', '').replaceAll('>', '').replaceAll('p', '');
+    final image = listOfFirstNews[0];
+    String content = "";
+
+    for (String s in listOfFirstNews.sublist(1, listOfFirstNews.length-1)){
+      content += s.replaceAll('<', '').replaceAll('>', '').replaceAll('p', '').replaceAll('br', '').replaceAll('/', '').replaceAll('\\', '').replaceAll('\n', '').replaceAll('&nbs;', '');
+      content += '\n';
+    }
+    print(title);
+    print(description);
+    print(image);
+    print(content);
+  });
+
+
+}
